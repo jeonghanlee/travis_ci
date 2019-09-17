125b728e65add452378583ce7cbe74b8fdc166df
Jeong Han Lee <jeonghan.lee@gmail.com>
2019-09-17 13:10:03 +0200

    tune repo

--- setup_repo.bash
+++ setup_repo.bash
@@ -5,3 +5,8 @@ export PATH=$HOME/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > $HOME/bin/repo
 chmod a+x $HOME/bin/repo
 
+# Mandatory within travis
+
+git config --global color.ui false
+
+
